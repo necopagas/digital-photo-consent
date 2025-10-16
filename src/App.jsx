f299752878e887c655064bccd33c1202b3ccaac0
necopagas <necopagas@gmail.com>
2025-10-16 19:37:57 +0800

    feat: Finalize transition to local PDF generation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import ConsentForm from './ConsentForm.jsx';
-import AdminView from './AdminView.jsx';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ConsentForm />} />
-          <Route path="/admin" element={<AdminView />} />
         </Routes>
       </BrowserRouter>
       <Toaster position="top-center" reverseOrder={false} /> 
